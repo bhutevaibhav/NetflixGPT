70caa169c1bea456c35908c45c97a0d4558ad8e6
Vaibhav <bhutevaibhav508@gmail.com>
2026-06-16 20:45:10 +0530

    create sign-in form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import "./App.css";
-
+import Body from "./components/Body";
 function App() {
-  return <h1 className="text-3xl font-bold underline bg-red-400">Hello</h1>;
+  return (
+    <div>
+      <Body />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Login.jsx
@@ -0,0 +1,37 @@
+import Header from "./Header";
+
+const Login = () => {
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="background-img"
+        />
+      </div>
+
+      <form className="absolute w-4/12 p-12 bg-black/80 my-36 mx-auto right-0 left-0 text-white bg-opacity-80">
+        <h1 className="text-2xl font-bold">Sign In</h1>
+        <input
+          type="text"
+          placeholder="Email"
+          className="p-4 my-4 bg-gray-800 w-full"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 bg-gray-800 w-full"
+        />
+        <button type="submit" className="p-3 my-6 w-full bg-red-600 text-white">
+          Sign In
+        </button>
+        <p>
+          New to Netflix? <span className="text-gray-400">Sign up now.</span>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
