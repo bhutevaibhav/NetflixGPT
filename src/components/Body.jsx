8a9b81ae3ccf92e5064b7a204bb74c021ded20dc
Vaibhav <bhutevaibhav508@gmail.com>
2026-06-19 20:16:23 +0530

    implement the reduxt store and dispatch actions

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter, RouterProvider } from "react-router";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/firebase";
 import Browse from "./Browse";
 import Login from "./Login";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../store/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
   const appRouter = createBrowserRouter([
     {
       path: "/",
@@ -13,6 +19,18 @@ const Body = () => {
       element: <Browse />,
     },
   ]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email } = user;
+        dispatch(addUser({ uid, email }));
+      } else {
+        dispatch(removeUser());
+      }
+    });
+  }, []);
+
   return (
     <div>
       <RouterProvider router={appRouter} />
